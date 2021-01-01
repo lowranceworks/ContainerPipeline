6615e232cbc9fcf3a81519341e8e99a477cb78bc
Josh Lowrance <38927314+lowranceworks@users.noreply.github.com>
2021-01-01 14:57:14 -0600

    Update container_pipeline-stack.ts

--- lib/container_pipeline-stack.ts
+++ lib/container_pipeline-stack.ts
@@ -1,14 +1,20 @@
-import * as cdk from '@aws-cdk/core';
-import * as codecommit from '@aws-cdk/aws-codecommit';
-import * as targets from '@aws-cdk/aws-events-targets';
-import * as codebuild from '@aws-cdk/aws-codebuild';
-import * as ecr from '@aws-cdk/aws-ecr';
-import * as codedeploy from '@aws-cdk/aws-codedeploy';
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as pipeline from '@aws-cdk/aws-codepipeline';
-// import { pipeline } from 'stream';
-// import * as events from '@aws-cdk/aws-events';
+import * as path from "path";
+import * as codeCommit from "@aws-cdk/aws-codecommit";
+import * as codeBuild from "@aws-cdk/aws-codebuild";
+import * as codeArtifact from "@aws-cdk/aws-codeartifact";
+import * as codeDeploy from "@aws-cdk/aws-codedeploy";
+import * as codePipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipelineActions from "@aws-cdk/aws-codepipeline-actions";
+import * as ecr from "@aws-cdk/aws-ecr";
+import * as cloudWatch from "@aws-cdk/aws-cloudwatch";
+import * as cloudTrial from "@aws-cdk/aws-cloudtrail";
+import * as sns from "@aws-cdk/aws-sns";
+import * as iam from "@aws-cdk/aws-iam";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as subs from "aws-cdk/aws-sns-subscriptions";
+import * as events from "aws-cdk/aws-events";
+import * as targets from "aws-cdk/aws-events-targets";
+import { App, Stack, StackProps } from "@aws-cdk/core";
 
 export class ContainerPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
